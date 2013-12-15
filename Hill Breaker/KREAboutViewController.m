f0389b5b9d8daa4231aba4089100ebc3164010f2
Alex <alex.krebiehl@gmail.com>
2013-12-15 10:28:58 -0500

    Added license information to the UI

--- Hill Breaker/KREAboutViewController.m
+++ Hill Breaker/KREAboutViewController.m
@@ -3,7 +3,21 @@
 //  Hill Breaker
 //
 //  Created by Alex Krebiehl on 12/15/13.
-//  Copyright (c) 2013 Alex Krebiehl. All rights reserved.
+//  Copyright (c) 2013 Alex Krebiehl
+//
+//  This program is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; either version 2 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License along
+//  with this program; if not, write to the Free Software Foundation, Inc.,
+//  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 //
 
 #import "KREAboutViewController.h"
@@ -19,8 +33,24 @@
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
         // Initialization code here.
+        
+
     }
     return self;
+}
+
+- (void)loadView
+{
+    [super loadView];
+    
+    // Load the license from file
+    NSString *path = [[NSBundle mainBundle] pathForResource:@"LICENSE"
+                                                     ofType:@""];
+    NSString *license = [NSString stringWithContentsOfFile:path
+                                                  encoding:NSUTF8StringEncoding
+                                                     error:NULL];
+    self.licenseTextView.string = license;
+    
 }
 
 @end
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 Hill-Breaker
 ============
+
+At some point I will add program flow information here so that way you can easily trace through the source.
